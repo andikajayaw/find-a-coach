bc4a367e7e7911af4c1452dd48ec9639a5a19511
User <andikajayawiguna@gmail.com>
2021-07-07 22:34:33 +0800

    Add Coach Item component & update Coaches List

--- src/Pages/Coaches/CoachesList.vue
+++ src/Pages/Coaches/CoachesList.vue
@@ -7,16 +7,29 @@
     </div>
     <ul v-if="hasCoaches">
       LIST OF COACHES
-      <li v-for="coach in filteredCoaches" :key="coach.id">
+      <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
-      </li>
+      </li> -->
+      <CoachItem
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :areas="coach.areas"
+        :rate="coach.hourlyRate"
+      />
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       //   console.log(this.$store.getters['coaches/coaches']);
@@ -29,3 +42,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
