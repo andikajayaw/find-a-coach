c4fcf46db12164bd15018cbde7963358ac53e496
User <andikajayawiguna@gmail.com>
2021-08-02 22:07:43 +0800

    add put method with firebase on actions/vuex

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,26 @@
 export default {
-    registerCoach(context, data) {
+    async registerCoach(context, data) {
         console.table(data);
+        const userId = context.rootGetters.userId;
         const coachData = {
-            id: context.rootGetters.userId,
             firstName: data.firstName,
             lastName: data.lastName,
             hourlyRate: data.hourlyRate,
             description: data.description,
             areas: data.areas,
+        };
+
+        const response = await fetch(`https://find-a-coach-vue-1befc-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        })
+
+        // const responseData = await response.json();
+
+        if (!response.ok) {
+            // error
         }
-        context.commit('registerCoach', coachData);
+
+        context.commit('registerCoach', { ...coachData, id: userId });
     }
 };
