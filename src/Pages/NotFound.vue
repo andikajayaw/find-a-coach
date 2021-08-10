7682c0835c3c7592a1e5634c86267cf98e668f0f
andikajayaw <andikajayawiguna@gmail.com>
2021-08-10 23:53:05 +0800

    add Not Found page

---
+++ src/Pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not Found!</h2>
+      <p>
+        This page could not this found. Check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
