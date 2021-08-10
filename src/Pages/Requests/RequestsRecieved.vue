ef0a8388b908c029cd8cc1d83cde8022731f8a76
andikajayaw <andikajayawiguna@gmail.com>
2021-08-10 23:06:41 +0800

    sending coach requests with http requests (firebase)

--- src/Pages/Requests/RequestsRecieved.vue
+++ src/Pages/Requests/RequestsRecieved.vue
@@ -1,10 +1,16 @@
 <template>
+  <base-dialog :show="!!error" title="Error!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Request Recieved</h2>
       </header>
-      <ul v-if="hasRequests">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasRequests && !isLoading">
         <RequestItems
           v-for="req in recievedRequests"
           :key="req.id"
@@ -23,12 +29,35 @@ export default {
   components: {
     RequestItems,
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     recievedRequests() {
       return this.$store.getters['requests/requests'];
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Failed to fetch';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
