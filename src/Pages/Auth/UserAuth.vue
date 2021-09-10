3bd3d3b3893a04ff75bdd7a9d48b589bfa54550f
andika jaya <andikajayawiguna@gmail.com>
2021-09-10 23:36:12 +0800

    add login function

--- src/Pages/Auth/UserAuth.vue
+++ src/Pages/Auth/UserAuth.vue
@@ -69,17 +69,19 @@ export default {
         this.formIsValid = false;
         return;
       }
+
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
       this.isLoading = true;
 
       try {
         // send http request
         if (this.mode === 'login') {
-          //
+          await this.$store.dispatch('login', actionPayload);
         } else {
-          await this.$store.dispatch('signup', {
-            email: this.email,
-            password: this.password,
-          });
+          await this.$store.dispatch('signup', actionPayload);
         }
       } catch (error) {
         this.error = error.message || 'Failed to Sign Up';
